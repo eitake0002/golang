b3a33544e7f0788a4043c1246e1cb49de5c5c0d2
root <eiki.e.t.d@gmail.com>
2016-04-10 00:01:39 +0900

    Implement get request.

--- read_file.go
+++ read_file.go
@@ -7,6 +7,8 @@ import (
   "os"
   "bufio"
   "reflect"
+  "net/http"
+  "io/ioutil"
 )
 
 type MyWrapper struct{
@@ -25,6 +27,18 @@ func (f *MyWrapper) ReadFile() []string {
   return content
 }
 
+func (f *MyWrapper) GetRequest(url string) string {
+  resp, err := http.Get(url)
+  if err != nil {
+    log.Print(err)
+  }
+  log.Print(resp)
+  body, err := ioutil.ReadAll(resp.Body)
+  //log.Print(body)
+  log.Print(reflect.TypeOf(body))
+  return string(body)
+}
+
 func main(){
   start := time.Now()
   log.Print("Start")
@@ -34,6 +48,8 @@ func main(){
   fmt.Println(reflect.TypeOf(wrapper))
   content := wrapper.ReadFile()
   fmt.Println(content)
+  body := wrapper.GetRequest("http://your-news.me")
+  log.Print(body)
 
   end := time.Now()
   log.Printf("%f sec\n", (end.Sub(start)).Seconds())
