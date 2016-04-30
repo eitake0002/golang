cec666597d2304f985401008733139da03d62caf
root <eiki.e.t.d@gmail.com>
2016-04-30 22:57:54 +0900

    Add get urls with wget shell script.

--- get_urls.sh
+++ get_urls.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-wget -q http://google.com -O - | \
+wget -r -l 1 -H -q http://google.com -O - | \
     tr "\t\r\n'" '   "' | \
     grep -i -o '<a[^>]\+href[ ]*=[ \t]*"\(ht\|f\)tps\?:[^"]\+"' | \
     sed -e 's/^.*"\([^"]\+\)".*$/\1/g'
