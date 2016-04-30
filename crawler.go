1bbadf8b1a05283b3cb1f214974de959bd2d0bc3
root <eiki.e.t.d@gmail.com>
2016-04-30 23:55:24 +0900

    Implement recursive crawl and exec go fmt to crawler.

--- crawler.go
+++ crawler.go
@@ -1,41 +1,64 @@
 package main
 
-import(
-  "log"
-  _ "fmt"
-  "time"
-  _ "net/http"
-  _ "io/ioutil"
-  _ "bufio"
-  _ "os"
-  _ "os/exec"
+import (
+	_ "bufio"
+	_ "fmt"
+	_ "io/ioutil"
+	"log"
+	_ "net/http"
+	_ "os"
+	_ "os/exec"
+	"regexp"
+	"time"
 
-  "github.com/PuerkitoBio/goquery"
+	"github.com/PuerkitoBio/goquery"
 )
 
 func Scraping(url string) []string {
-  doc, err := goquery.NewDocument(url)
-  if err != nil {
-    log.Fatal(err)
-  }
+	doc, err := goquery.NewDocument(url)
+	if err != nil {
+		log.Fatal(err)
+	}
 
-  var url_list []string
-  doc.Find("a").Each(func(_ int, s *goquery.Selection){
-    url, _ := s.Attr("href")
-    url_list = append(url_list, url) 
-  })
-  return url_list
+	var url_list []string
+	doc.Find("a").Each(func(_ int, s *goquery.Selection) {
+		url, _ := s.Attr("href")
+		r := regexp.MustCompile(`(https://|http://)`)
+		if r.MatchString(url) {
+			url_list = append(url_list, url)
+		}
+	})
+	return url_list
+}
+
+func Output2Array(ary [][]string) {
+  for _, v := range ary {
+    for _, v2 := range v {
+      log.Println(v2)
+    }
+  }
 }
 
 func main() {
-  log.Println("Start")
-  start := time.Now();
+	log.Println("Start")
+	start := time.Now()
 
-  url_list := Scraping("http://google.com")
-  for _, v := range url_list {
-    log.Println(v)
-  }
+	var url_list [][]string
+	for i := 0; i < 3; i++ {
+		log.Println(i)
+		var urls []string
+		if i == 0 {
+			urls = Scraping("http://yahoo.co.jp")
+		} else {
+			for _, v := range url_list[i-1] {
+				urls = Scraping(v)
+			}
+		}
+		url_list = append(url_list, urls)
+	}
 
-  end := time.Now();
-  log.Printf("%f秒\n",(end.Sub(start)).Seconds())
+    Output2Array(url_list)
+
+	end := time.Now()
+	log.Printf("%f秒\n", (end.Sub(start)).Seconds())
 }
