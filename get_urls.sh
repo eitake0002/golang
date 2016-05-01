e1401d3e4409d3a0bed0be5c3bf1f0d911f63b2d
root <eiki.e.t.d@gmail.com>
2016-05-01 14:14:12 +0900

    clean files.

--- get_urls.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-wget -r -l 1 -H -q http://google.com -O - | \
-    tr "\t\r\n'" '   "' | \
-    grep -i -o '<a[^>]\+href[ ]*=[ \t]*"\(ht\|f\)tps\?:[^"]\+"' | \
-    sed -e 's/^.*"\([^"]\+\)".*$/\1/g'
